8213a4e89c9e5910bd6055645bd97ccfa6efc4c7
Subitsha <subikarthika14@gmail.com>
2024-01-18 18:13:44 +0530

    First commit

---
+++ RailwayTicketBooking/App.java
@@ -0,0 +1,9 @@
+package RailwayTicketBooking;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Railway Ticket Booking :)");
+        TicketBooking t = new TicketBooking();
+        t.menu();
+    }
+}
